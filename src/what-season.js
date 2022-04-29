dabe5c67e2f5cf1035810e3e1a75e12fdbb70154
Tabunov Egor <Vonubat@gmail.com>
2022-04-29 05:46:11 +0300

    feat: final solution for "What season" task

--- src/what-season.js
+++ src/what-season.js
@@ -11,30 +11,85 @@ const { NotImplementedError } = require("../extensions/index.js");
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  *
  */
-function getSeason(date) {
-  const seasons = ["winter", "spring", "summer", "autumn"];
-  let month = date.getMonth();
 
-  if (typeof date.getMonth !== "function") {
+function getSeason(date) {
+  // console.log(Object.getOwnPropertyNames(date));
+  if (!date) {
     return "Unable to determine the time of year!";
   }
-
-  if (!(date instanceof Date && !isNaN(date))) {
+  if (
+    arguments.length > 0 &&
+    (date instanceof Date === false ||
+      Object.getOwnPropertyNames(date).length > 0)
+  ) {
     throw new Error("Invalid date!");
   }
 
-  if ((month >= 0 && month < 2) || month == 11) {
-    return seasons[0];
-  } else if (month >= 2 && month < 5) {
-    return seasons[1];
-  } else if (month >= 5 && month < 8) {
-    return seasons[2];
-  } else if (month >= 8 && month < 11) {
-    return seasons[3];
+  if (date.getMonth() === 2 || date.getMonth() === 3 || date.getMonth() === 4) {
+    return "spring";
+  } else if (
+    date.getMonth() === 5 ||
+    date.getMonth() === 6 ||
+    date.getMonth() === 7
+  ) {
+    return "summer";
+  } else if (
+    date.getMonth() === 8 ||
+    date.getMonth() === 9 ||
+    date.getMonth() === 10
+  ) {
+    return "autumn";
+  } else {
+    return "winter";
   }
 }
 
-console.log(getSeason(new Date(1556, 3, 9, 21, 17, 46, 991)));
+// const fakeDate = {
+//   toString() {
+//     return Date.prototype.toString.call(new Date());
+//   },
+//   [Symbol.toStringTag]: "Date",
+// };
+
+// Object.setPrototypeOf(fakeDate, Object.getPrototypeOf(new Date()));
+
+// getSeason(fakeDate)
+
+// const deeperFakeDate = {
+//   toString() {
+//     return Date.prototype.toString.call(new Date());
+//   },
+//   getMonth() {
+//     return Date.prototype.getMonth.call(new Date());
+//   },
+//   getFullYear() {
+//     return Date.prototype.getFullYear.call(new Date(1994, 1, 2, 3, 4, 5));
+//   },
+//   getDate() {
+//     return Date.prototype.getDate.call(new Date(2020, 0, 3, 4, 5, 6));
+//   },
+//   getHours() {
+//     return Date.prototype.getHours.call(new Date(1978, 2, 4, 5, 6, 7));
+//   },
+//   getMinutes() {
+//     return Date.prototype.getMinutes.call(new Date(202, 3, 5, 6, 7, 8));
+//   },
+//   getSeconds() {
+//     return Date.prototype.getSeconds.call(new Date(1, 4, 6, 7, 8, 9));
+//   },
+//   getMilliseconds() {
+//     return Date.prototype.getMilliseconds.call(new Date(2019, 7, 8, 9, 10, 11));
+//   },
+//   getDay() {
+//     return Date.prototype.getDay.call(new Date(1812, 8, 9, 10, 11, 12));
+//   },
+//   [Symbol.toStringTag]: "Date",
+// };
+
+// Object.setPrototypeOf(deeperFakeDate, Object.getPrototypeOf(new Date()));
+
+// getSeason(deeperFakeDate);
+
 module.exports = {
   getSeason,
 };
