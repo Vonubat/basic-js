c30c09f48ec856663b02bf960ae67ebc05c9a54b
Tabunov Egor <Vonubat@gmail.com>
2022-05-01 15:02:55 +0300

    feat: solution for -> "Recursive depth calculator" task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -14,65 +14,28 @@ const { NotImplementedError } = require("../extensions/index.js");
  */
 
 class DepthCalculator {
-  calculateDepth(arr) {
-    let counter = 1;
-    // let flattedArr = [];
-
-    // count max depth (without recursion)
-
-    for (let i = 0; i < 30; i++) {
-      let temp = arr.flat(i);
-      let arrCheck = 0;
-      for (let item of temp) {
-        if (Array.isArray(item)) {
-          arrCheck++;
-        }
-      }
-      if (arrCheck > 0) {
-        counter++;
-      }
+  calculateDepth(arr, depthCount) {
+    if (!depthCount) {
+      depthCount = 0;
     }
+    depthCount++;
 
-    // implement .flat()
-    // function recursion(arr) {
-    //   let check = 0;
-    //   for (let item of arr) {
-    //     if (!Array.isArray(item)) {
-    //       flattedArr.push(item);
-    //     } else {
-    //       check++;
-    //       recursion(item);
-    //     }
-    //   }
-    //   console.log(check);
-    //   if (check == 1) {
-    //     counter++;
-    //   }
-    // }
+    const depthArray = new Array(arr.length || 1);
+    depthArray.fill(depthCount);
+    console.log(depthArray);
+    arr.forEach((element, index) => {
+      if (Array.isArray(element)) {
+        depthArray[index] = this.calculateDepth(element, depthCount);
+      }
+    });
 
-    // recursion(arr);
-    // console.log(flattedArr);
-    // console.log(counter);
-
-    return counter;
+    const result = Math.max(...depthArray);
+    return result;
   }
 }
 
-const depthCalc = new DepthCalculator();
-
-// depthCalc.calculateDepth([1, 2, 3, 4, 5]);
-
-depthCalc.calculateDepth([
-  1,
-  2,
-  3,
-  4,
-  [1, 2, [1, 2, [[[]]]]],
-  5,
-  [1, [[[[[[]]]]]]],
-]);
-
-// depthCalc.calculateDepth([[[]]]);
+let calculator = new DepthCalculator();
+calculator.calculateDepth([1, [8, [[]]], 2, 3, [8, []], 4, 5, []]);
 
 module.exports = {
   DepthCalculator,
